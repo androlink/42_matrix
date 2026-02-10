6e82adc4aac3fb03ec44e8717564f696f390977d
andro <guilhem.cros.38@gmail.com>
2026-02-10 13:15:51 +0100

    add test

--- src/vector/add.rs
+++ src/vector/add.rs
@@ -5,3 +5,17 @@ impl<K: std::ops::AddAssign + Copy, const N: usize> Vector<K, N> {
         *self += *v;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut v1 = Vector::from([2., 3.]);
+        let v2 = Vector::from([5., 7.]);
+        let res = Vector::from([7., 10.]);
+        v1.add(&v2);
+        assert_eq!(v1, res, "add {:?} and {:?}", v1, res);
+    }
+}
